0b6b9eebcd84e505edff487ee1f9fc3967f0a9ed
imsrkulkarni <77853418+imsrkulkarni@users.noreply.github.com>
2022-03-01 18:32:07 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -5,8 +5,8 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
-    int i;
-    float sum=0.0,maximum,minimum;
+   
+    float i,sum=0.0,maximum,minimum;
     /* maximum in the given number set */
     maximum=numberset[0];
     for(i=1;i<setlength;i++)
