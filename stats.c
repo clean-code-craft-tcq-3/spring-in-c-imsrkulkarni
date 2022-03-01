cec296271e030c07abd1b4e512498a37782a86ef
imsrkulkarni <77853418+imsrkulkarni@users.noreply.github.com>
2022-03-01 17:45:20 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -5,7 +5,50 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    int i,j;
+    float temp=0.0,sum=0.0;
+    /* Sorting the given number set */
+    for(i=0;i<setlength-1;i++)
+    {
+        for(j=i+1;j<setlength;j++)
+        {
+            if(numberset[i]>numberset[j])
+            {
+                temp=numberset[i];
+                numberset[i]=number[j];
+                numberset[j]=temp;
+            }
+         }
+    }
+    s.min=numberset[0];
+    s.max=numberset[setlength-1];
+    
+    /* Finding Average of the given number set */
+    for(i=0;i<setlength;i++)
+    {
+        sum+=numberset[i];
+    }
+    s.average=sum/setlength;
+    return s;
 }
 
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
+void emailAlerter()
+{
+    emailAlertCallCount++;
+}
+void ledAlerter()
+{
+    ledAlertCallCount++;
+}
+
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
+{
+    int maximum= computedStats.max;
+    if(maxThreshold < maximum)
+    {
+        (*alerters[0])();
+        (*alerters[1])();
+    }
+}  
